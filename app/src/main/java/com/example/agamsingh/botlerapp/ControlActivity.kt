e13f89a82e03f48cafacdf1e6031eaad4f9f7378
Agamjyot Singh <agamjyotsinghn@gmail.com>
2018-10-12 14:56:45 +0200

    Added click listeners to the arrows

--- app/src/main/java/com/example/agamsingh/botlerapp/ControlActivity.kt
+++ app/src/main/java/com/example/agamsingh/botlerapp/ControlActivity.kt
@@ -6,11 +6,36 @@ import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.util.AttributeSet
 import android.view.View
+import kotlinx.android.synthetic.main.control_layout.*
 
 class ControlActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.control_layout)
+
+        right_arrow.setOnClickListener {
+            moving.visibility = View.VISIBLE
+            moving.text = "Botler moving right"
+        }
+
+
+        left_arrow.setOnClickListener {
+            moving.visibility = View.VISIBLE
+            moving.text = "Botler moving left"
+        }
+
+
+        down_arrow.setOnClickListener {
+            moving.visibility = View.VISIBLE
+            moving.text = "Botler moving down"
+        }
+
+
+        up_arrow.setOnClickListener {
+            moving.visibility = View.VISIBLE
+            moving.text = "Botler moving up"
+        }
     }
+
 }
